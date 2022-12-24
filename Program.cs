58574703f35e225b685c7b105daff9b7c94c1d03
maryamabdallahhh <121059049+maryamabdallahhh@users.noreply.github.com>
2022-12-24 13:18:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-  
+
+
+Console.Write("enter the first number : ");
+int num1 = int.Parse(Console.ReadLine());
+Console .Write("enter the second number : ");
+int num2= int.Parse(Console.ReadLine());
+Console.WriteLine("the prime numbers are : ");
+    for (int i = num1; i < num2; i++)
+    {
+    int j;
+    for (j = 2; j < i; j++)
+    {
+        if (i % j == 0) break;
+    }
+        if (j == i) Console.WriteLine("{0}, ", i);
+
+    }
+    
+
